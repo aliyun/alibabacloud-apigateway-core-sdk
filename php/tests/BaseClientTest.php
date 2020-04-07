4e2737b857dfe4df76a6886a5a6d6b1f34d75f43
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-04-07 13:18:39 +0800

    ApiGateway BaseClient for php

---
+++ php/tests/BaseClientTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AlibabaCloud\ApiGateway\SDK\Tests;
+
+use AlibabaCloud\ApiGateway\SDK\BaseClient;
+use AlibabaCloud\Tea\Request;
+use PHPUnit\Framework\TestCase;
+
+class BaseClientTest extends TestCase
+{
+    private $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->client = new BaseClient();
+    }
+
+    public function testGetSignature()
+    {
+        $request           = new Request();
+        $request->method   = "";
+        $request->pathname = "";
+        $request->query    = [
+            "sdk" => "apigateway"
+        ];
+        $request->headers  = [
+            "baseclient" => "go"
+        ];
+        $this->assertEquals("h3zZzWDRJ+OiWSlhFl1YKhOvk5hOfxxOVIeH9kV86vw=", $this->client->getSignature($request, ""));
+    }
+
+    public function testToQuery()
+    {
+        $query = [
+            "test" => "ok",
+            "null" => null
+        ];
+        $res   = $this->client->toQuery($query);
+        $this->assertEquals("ok", $res["test"]);
+        $this->assertFalse(isset($res["null"]));
+    }
+
+    public function testIsFail()
+    {
+        $this->assertFalse($this->client->isFail(200));
+        $this->assertTrue($this->client->isFail(400));
+    }
+
+    public function testGetContentMD5()
+    {
+        $this->assertEquals("b969h28MOfCVGrra1smdCg==", $this->client->getContentMD5('{"test":"ok"}'));
+    }
+}
